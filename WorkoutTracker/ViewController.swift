c2bca041a8d717cde2fa9b66d66611627173fcb4
Chris Allen <callen7908@yahoo.com>
2021-05-25 07:09:49 -0400

    Started work on first screen

--- WorkoutTracker/ViewController.swift
+++ WorkoutTracker/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numWorkoutsLab: UILabel!
+    @IBOutlet weak var startWorkoutBut: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
